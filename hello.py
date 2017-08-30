942dbbb1aad60d5e19a4f988b1e7d39b3d1db9d6
KennethA2001 <31477599+KennethA2001@users.noreply.github.com>
2017-08-30 09:09:27 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,10 @@
+# Kenneth Alexander
+# August 24, 2017
+
+print("Hello.")
+print("What is your name?")
+name = input()
+print("It is good to meet you, " + name + ".")
+print("Where were you born," + name + "?")
+location=input()
+print("That's interesting. I'd like to visit " + location + " one day.")
