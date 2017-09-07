197d9a6b9bae2eaa448f18eb266fe977903c891b
KennethA2001 <31477599+KennethA2001@users.noreply.github.com>
2017-09-07 16:21:03 -0400

    Add files via upload

--- Intro to Python Review.py
+++ Intro to Python Review.py
@@ -11,3 +11,15 @@ def surface_area_cylinder(r, h):
 # function calls
 print(surface_area_cylinder(4,10))
 
+def fahrenheit_to_celsius(f):
+    c= (f-32)*5/9
+    return c
+# function calls
+print(fahrenheit_to_celsius(80))
+import math
+sqrt=3**1/2
+print(sqrt)
+
+print("What is your favorite color? ")
+fav_color=input()
+print("My favorite color is " + str(fav_color)+ ".")
