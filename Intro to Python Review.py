489a5bacae61eca555e498d1a4302867f84fa3bc
KennethA2001 <31477599+KennethA2001@users.noreply.github.com>
2017-09-07 09:33:18 -0400

    Add files via upload

--- Intro to Python Review.py
+++ Intro to Python Review.py
@@ -1,3 +1,13 @@
 print('The knights who say,"Ni!"')
+year=2001
+print("Kenneth was born in "  + str(year))
 
-      
+import math
+
+def surface_area_cylinder(r, h):
+    a= 2 * math.pi * r * h + 2 * math.pi * r ** 2
+    return a
+
+# function calls
+print(surface_area_cylinder(4,10))
+
